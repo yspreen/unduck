dacf27e41bbcf0c75ae9d57186872000d31be270
Nick Spreen <12631527+yspreen@users.noreply.github.com>
2025-04-04 09:33:25 -0400

    Change links.

--- src/main.ts
+++ src/main.ts
@@ -21,11 +21,11 @@ function noSearchDefaultPageRender() {
         </div>
       </div>
       <footer class="footer">
-        <a href="https://t3.chat" target="_blank">t3.chat</a>
+        <a href="https://quanta.tools" target="_blank">quanta</a>
         •
-        <a href="https://x.com/theo" target="_blank">theo</a>
+        <a href="https://spreen.co" target="_blank">spreen.co</a>
         •
-        <a href="https://github.com/t3dotgg/unduck" target="_blank">github</a>
+        <a href="https://github.com/yspreen/unduck" target="_blank">github</a>
       </footer>
     </div>
   `;
